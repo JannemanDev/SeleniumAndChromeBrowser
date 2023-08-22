5aaf89d5cd0f78a7260fd021211f8e34cdd8aff6
Jan Oonk <J.Oonk@fontys.nl>
2023-08-22 10:01:50 +0200

    added handy helper batch file

---
+++ killTaskUsingPort.bat
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "port=%~1"
+set "found=0"
+set "output="
+
+for /f "delims=" %%a in ('netstat -aon -p TCP ^| findstr "LISTENING" ^| findstr ":%port%"') do (
+    set "output=!output!%%a"
+)
+
+echo Searching for processes listening on port %port%...
+if "%output%" neq "" (
+    echo %output%
+    for /f "tokens=5" %%a in ('echo %output%') do (
+        set "pid=%%a"
+        set "found=1"
+        goto :Terminate
+    )
+)
+
+:Terminate
+if %found% equ 1 (
+    echo Terminating process with PID %pid% that is LISTENING on port %port% using TCP protocol...
+    taskkill /F /PID %pid%
+) else (
+    echo No process found LISTENING on port %port% using TCP protocol.
+)
+
+endlocal
