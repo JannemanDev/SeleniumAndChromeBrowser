247e20458c0741aef1b092077a153083bf7e18bc
Jan Oonk <J.Oonk@fontys.nl>
2023-08-23 13:21:47 +0200

    refactored some code and publish fix

---
+++ Source/SeleniumAndChromeBrowser/NetstatOutputDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumAndChromeBrowser
+{
+    internal class NetstatOutputDefinition
+    {
+        public string Command { get; set; }
+        public string Arguments { get; set; }
+        public int NumColumns { get; set; }
+        public int ProtoColumnIndex { get; set; }
+        public int LocalAddressColumnIndex { get; set; }
+        public int ForeignAddressColumnIndex { get; set; }
+        public int StateColumnIndex { get; set; }
+        public int PidColumnIndex { get; set; }
+        public string State { get; set; }
+    }
+}
--- publishNewRelease.bat
+++ publishNewRelease.bat
@@ -1,6 +1,8 @@
 @echo off
+setlocal
 
 set TAG_NAME=v1.1
+set "VERSION=%TAG_NAME:~1%"
 set RELEASE_TITLE=Release %TAG_NAME%
 set RELEASE_NOTES=
 set dotnetcore=net6.0
@@ -8,4 +10,6 @@ set dotnetcore=net6.0
 :: Create the release
 gh release create %TAG_NAME% --title "%RELEASE_TITLE%" --notes "%RELEASE_NOTES%"
 
-call uploadFilesForRelease.bat %TAG_NAME% .\Builds\%dotnetcore%\*.zip
+call uploadFilesForRelease.bat %TAG_NAME% .\Builds\%dotnetcore%\*-%VERSION%-*.zip
+
+endlocal
