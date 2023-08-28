f9fe5a75ece4f5a073f4526329dab028b9bb820a
Jan Oonk <J.Oonk@fontys.nl>
2023-08-28 12:23:59 +0200

    update
    -added override profilepath to startChrome.bat

--- startChrome.bat
+++ startChrome.bat
@@ -1,3 +1,11 @@
 @echo off
-echo Starting Chrome...
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+set "userDataDir=%1"
+if not defined userDataDir (
+	echo No userDataDir as commandline argument was given^!
+	echo Starting Chrome...
+)
+if defined userDataDir (
+	echo Starting Chrome using profile located at %userDataDir%
+)
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --user-data-dir=%userDataDir%
