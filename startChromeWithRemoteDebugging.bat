5d26ed7d563c3bfc71f3c24096705964f7055457
Jan Oonk <J.Oonk@fontys.nl>
2023-08-28 11:57:21 +0200

    update
    -startchrome helper batchfile with override profilepath

--- startChromeWithRemoteDebugging.bat
+++ startChromeWithRemoteDebugging.bat
@@ -8,26 +8,31 @@ if not defined port (
 	echo No port as commandline argument was given^^! Using default port !port!
 )
 
-rem Check if the environment variable is defined
-if defined TEMP (
-    rem Check if the directory exists
-    if not exist "%TEMP%" (
-        echo TEMP Directory does not exist: %TEMP%
-		exit /b 1
-    )
-) else (
-    echo TEMP Environment variable is not defined: %TEMP%
-	exit /b 1
-)
+set "userDataDir=%2"
+if not defined userDataDir (
+	echo No userDataDir as commandline argument was given^^!
 
-if not "!TEMP:~-1!"=="\" (
-	set "userDataDir=%TEMP%\chrome-debug-profile-%port%"
-) else (
-	set "userDataDir=%TEMP%chrome-debug-profile-%port%"
+	rem Check if the environment variable is defined
+	if defined TEMP (
+		rem Check if the directory exists
+		if not exist "%TEMP%" (
+			echo TEMP Directory does not exist: %TEMP%
+			exit /b 1
+		)
+	) else (
+		echo TEMP Environment variable is not defined: %TEMP%
+		exit /b 1
+	)
+
+	if not "!TEMP:~-1!"=="\" (
+		set "userDataDir=%TEMP%\chrome-debug-profile-%port%"
+	) else (
+		set "userDataDir=%TEMP%chrome-debug-profile-%port%"
+	)
 )
 
 echo Chrome will be started on port %port% using profile located at %userDataDir%
 
 @REM Always use an user data dir (profile) so it works when multiple Chrome instances are used (with or without specific debugging port)
-start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=%port% --user-data-dir="%userDataDir%"
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=%port% --user-data-dir=%userDataDir%
 exit /b 0
